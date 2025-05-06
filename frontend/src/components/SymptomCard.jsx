95e382e818b3b73127548d7be1ddd4f597909eff
Nando <kommandernando@outlook.com>
2025-05-06 18:53:15 -0400

    Added multipage modal and stlying, made minor changes to the symptom log logic and styling also

--- frontend/src/components/SymptomCard.jsx
+++ frontend/src/components/SymptomCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../styles/SymptomCard.css';
 
-export default function SymptomCard() {
+export default function SymptomCard({ log, handleDelete }) {
   return (
     <div className="symptom-card">
       <h3>Symptom Log</h3>
-      <p><strong>Symptoms:</strong> Rash, Bleeding, Swelling</p>
-      <p><strong>Pain Location:</strong> Arm</p>
-      <p><strong>Pain Level/Severity:</strong> 7</p>
-      <p><strong>Pain Type:</strong> Burning</p>
-      <p><strong>Doctor Type:</strong> Dermatologist, General Physician</p>
-      <p><strong>Notes:</strong> ________</p>
-      <p>Would you like to create an appointment?</p>
-      <div className="appointment-buttons">
-        <button className="create-appointment-btn">Create Appointment</button>
-      </div>
+      <p><strong>Date:</strong> {log.date}</p>
+      <p><strong>Symptoms:</strong> {log.symptoms.join(', ')}</p>
+      <p><strong>Pain Location:</strong> {log.painLocation.join(', ')}</p>
+      <p><strong>Pain Severity:</strong> {log.painSeverity}</p>
+      <p><strong>Pain Type:</strong> {log.painType.join(', ')}</p>
+      <p><strong>Doctor Type:</strong> {log.doctor.join(', ')}</p>
+      <button
+        className="delete-log-btn"
+        onClick={() => handleDelete(log)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
