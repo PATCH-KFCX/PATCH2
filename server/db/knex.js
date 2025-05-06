82f937dfedbc853a4382722d58d0c9c6a8f1406a
Marcy Student <Marcy_Student@MARCY-MBAir18.local>
2025-05-06 12:07:59 -0400

    I'm pushing my changes that I made, just copied down all of the files.

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,3 +1,9 @@
+// Load environment variables from the .env file
 const env = process.env.NODE_ENV || 'development';
+
+// Import the correct configuration based on the environment (development, production, etc.)
 const config = require('../knexfile')[env];
+
+// Export a new Knex instance using the selected environment config
+// This allows the rest of the app to use the database connection easily
 module.exports = require('knex')(config);
