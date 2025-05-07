123616ddf65a2db2ac7fae8afeccedb725ce6800
Marcy Student <Marcy_Student@MARCY-MBAir18.local>
2025-05-07 14:51:22 -0400

    I cleaned up some code

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -68,8 +68,7 @@ exports.seed = async (knex) => {
       pain_type: 'sharp',
       pain_location: 'forehead',
       pain_level: 5,
-      doctor_type: 'Primary Care',
-      other_notes: 'Felt worse in the morning',
+
     },
     {
       date: '2025-05-02',
@@ -77,8 +76,7 @@ exports.seed = async (knex) => {
       pain_type: 'tightness',
       pain_location: 'chest',
       pain_level: 7,
-      doctor_type: 'Cardiologist',
-      other_notes: 'After running up stairs',
+
     },
     {
       date: '2025-05-03',
@@ -86,8 +84,7 @@ exports.seed = async (knex) => {
       pain_type: 'aching',
       pain_location: 'lower abdomen',
       pain_level: 4,
-      doctor_type: 'Gastroenterologist',
-      other_notes: 'Felt after lunch',
+
     },
     {
       date: '2025-05-03',
@@ -95,8 +92,7 @@ exports.seed = async (knex) => {
       pain_type: 'dull',
       pain_location: 'behind eyes',
       pain_level: 3,
-      doctor_type: 'Allergist',
-      other_notes: 'Allergy season trigger',
+
     },
     {
       date: '2025-05-03',
@@ -104,8 +100,7 @@ exports.seed = async (knex) => {
       pain_type: 'chronic',
       pain_location: 'lower back',
       pain_level: 6,
-      doctor_type: 'Physical Therapist',
-      other_notes: 'After lifting boxes',
+
     },
     {
       date: '2025-05-03',
@@ -113,8 +108,7 @@ exports.seed = async (knex) => {
       pain_type: 'tightness',
       pain_location: 'lungs',
       pain_level: 5,
-      doctor_type: 'Pulmonologist',
-      other_notes: 'After walking stairs',
+
     },
     {
       date: '2025-05-04',
@@ -122,8 +116,7 @@ exports.seed = async (knex) => {
       pain_type: 'heat',
       pain_location: 'body',
       pain_level: 2,
-      doctor_type: 'Primary Care',
-      other_notes: 'Ongoing since yesterday',
+
     },
     {
       date: '2025-05-04',
@@ -131,8 +124,7 @@ exports.seed = async (knex) => {
       pain_type: 'sharp',
       pain_location: 'upper molar',
       pain_level: 8,
-      doctor_type: 'Dentist',
-      other_notes: 'Gets worse at night',
+
     },
     {
       date: '2025-05-04',
@@ -140,8 +132,7 @@ exports.seed = async (knex) => {
       pain_type: 'buzzing',
       pain_location: 'left ear',
       pain_level: 3,
-      doctor_type: 'ENT',
-      other_notes: 'Off and on for two days',
+
     },
     {
       date: '2025-05-04',
@@ -149,8 +140,7 @@ exports.seed = async (knex) => {
       pain_type: 'dull',
       pain_location: 'back of neck',
       pain_level: 4,
-      doctor_type: 'Chiropractor',
-      other_notes: 'After sleeping wrong',
+
     },
   ];
 
