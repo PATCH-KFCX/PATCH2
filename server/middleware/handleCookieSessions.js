0ddfb98d1806fad032e150c85c9f7075ba95be0f
Nando <kommandernando@outlook.com>
2025-05-21 18:33:50 -0400

    Fixing prod

--- server/middleware/handleCookieSessions.js
+++ server/middleware/handleCookieSessions.js
@@ -2,8 +2,8 @@ const cookieSession = require('cookie-session');
 
 module.exports = cookieSession({
   name: 'session',
-  secret: process.env.SESSION_SECRET || 'defaultsecret',
+  secret: process.env.SESSION_SECRET || 'patch_secret',
   maxAge: 24 * 60 * 60 * 1000,
   sameSite: 'lax',
-  secure: false,
+  secure: process.env.NODE_ENV === 'production', // true on Render
 });
