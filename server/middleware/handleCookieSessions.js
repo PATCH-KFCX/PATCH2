949d806c9f6f526a6bdfd73e3426be4c17ac2f80
Nando <kommandernando@outlook.com>
2025-05-21 18:52:33 -0400

    Fix env file

--- server/middleware/handleCookieSessions.js
+++ server/middleware/handleCookieSessions.js
@@ -4,6 +4,6 @@ module.exports = cookieSession({
   name: 'session',
   secret: process.env.SESSION_SECRET || 'patch_secret',
   maxAge: 24 * 60 * 60 * 1000,
+  secure: process.env.NODE_ENV === 'production',
   sameSite: 'lax',
-  secure: process.env.NODE_ENV === 'production', // true on Render
 });
