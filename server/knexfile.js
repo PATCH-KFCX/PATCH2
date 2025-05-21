3f78ebe99d33bcaa5dc11f325a5598d7d9932b2a
Nando <kommandernando@outlook.com>
2025-05-21 19:03:00 -0400

    Deploying

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const path = require('path');
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING || {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_DB,
+  },
+});
 
 const migrationsDirectory = path.join(__dirname, 'db/migrations');
 const seedsDirectory = path.join(__dirname, 'db/seeds');
 console.log('Connecting to DB:', process.env.PG_DB);
-
 
 module.exports = {
   development: {
